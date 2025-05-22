9852766639a69e1023701e7667133b9321043856
William Raendchen <william.raendchen@gmail.com>
2025-05-23 00:00:00 +0200

    cleanup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,29 +8,27 @@ import org.example.Peano.PeanoImpl;
 import org.example.Result.Err;
 import org.example.Result.Ok;
 
-import static org.example.Peano.PeanoImpl.*;
-
 public class Main {
     public static void main(String[] args) {
-        final Peano num1 = fromInt(5);
-        final Peano num2 = fromInt(3);
-        final Peano sum = add(num1, num2);
-        System.out.printf("Sum of %s and %s is: %s%n%n", toInt(num1), toInt(num2), toInt(sum));
+        final Peano num1 = Peano.fromInt(5);
+        final Peano num2 = Peano.fromInt(3);
+        final Peano sum = num1.add(num2);
+        System.out.printf("Sum of %s and %s is: %s%n%n", num1.toInt(), num2.toInt(), sum.toInt());
 
-        final Peano num0 = fromIntSafe(101).getOrThrow("It's the user's fault, I don't care!!!");
+        final Peano num0 = Peano.fromIntSafe(101).getOrThrow("It's the user's fault, I don't care!!!");
 
         // Explicit error handling. Compiler forces you to handle all possible cases.
-        final Peano num7 = switch(fromIntSafe(7)) {
+        final Peano num7 = switch(Peano.fromIntSafe(7)) {
             case Err(PeanoConstructionError.Negative err) -> throw new RuntimeException("Boo! Negative number");
             case Err(PeanoConstructionError.TooLarge err) -> throw new RuntimeException("Boo! Number too large");
             case Ok(Peano value) -> value;
         };
 
         // Force extract the value. Will blow up with less control over the error state.
-        final Peano num8 = fromIntSafe(8).getOrThrow("Yolo");
+        final Peano num8 = Peano.fromIntSafe(8).getOrThrow("Yolo");
 
         // Using a default value instead. Should almost *never* be used because it may create new bugs.
-        final Peano num9 = fromIntSafe(9).getOrElse(new Zero());
+        final Peano num9 = Peano.fromIntSafe(9).getOrElse(new Zero());
 
         final Peano num3 = switch (PeanoImpl.div(num1, num2)) {
             case Err(PeanoDivisionError.DivisionByZero err) -> throw new RuntimeException("Boo! Division by zero");
--- src/main/java/org/example/Result.java
+++ src/main/java/org/example/Result.java
@@ -8,7 +8,7 @@ sealed public interface Result<T, E> {
     default T getOrThrow(String justification) throws RuntimeException {
         return switch (this) {
             case Ok(T value) -> value;
-            case Err(E err) -> throw new RuntimeException(justification + ": " + err.toString());
+            case Err(E err) -> throw new RuntimeException("\n" + justification + " -> " + err.toString());
         };
     }
 
