7e4b56df4e9f62c3d572055459c580ee47c86ab3
William Raendchen <william.raendchen@gmail.com>
2025-05-23 00:00:00 +0200

    cleanup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,21 +3,22 @@ package org.example;
 import org.example.Peano.Peano;
 import org.example.Peano.PeanoError;
 import org.example.Peano.PeanoImpl;
+import org.example.Result;
 import org.example.Result.Err;
 import org.example.Result.Ok;
 
+import static org.example.Peano.PeanoImpl.fromInt;
+import static org.example.Peano.PeanoImpl.toInt;
+import static org.example.Peano.PeanoImpl.add;
+
 public class Main {
     public static void main(String[] args) {
-        final Peano num1 = PeanoImpl.fromInt(5);
-        final Peano num2 = PeanoImpl.fromInt(3);
+        final Peano num1 = fromInt(5);
+        final Peano num2 = fromInt(3);
+        final Peano sum = add(num1, num2);
+        System.out.printf("Sum of %s and %s is: %s%n%n", toInt(num1), toInt(num2), toInt(sum));
 
-        final Peano num3 = switch (PeanoImpl.div(num1, num2)) {
-            case Ok<Peano, PeanoError>(Peano peano) -> peano;
-            case Err<Peano, PeanoError>(PeanoError error) -> throw new IllegalArgumentException(error.toString());
-        };
-
-        final Peano num4 = PeanoImpl.add(num3, PeanoImpl.fromInt(2));
-
-        System.out.println("Result of addition: " + PeanoImpl.toInt(num4));
+        // final var num3 = PeanoImpl.div(num1, num2);
+        // final var num4 = PeanoImpl.add(num3, num2);
     }
 }
