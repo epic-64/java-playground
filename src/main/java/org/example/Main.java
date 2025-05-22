127e597611985a3022612adbfa19229a2ad5e39e
William Raendchen <william.raendchen@gmail.com>
2025-05-23 00:00:00 +0200

    cleanup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 import org.example.Peano.Peano;
 import org.example.Peano.PeanoError;
 import org.example.Peano.PeanoImpl;
+import org.example.Result.Err;
+import org.example.Result.Ok;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +12,8 @@ public class Main {
         final Peano num2 = PeanoImpl.fromInt(3);
 
         final Peano num3 = switch (PeanoImpl.div(num1, num2)) {
-            case Result.Ok<Peano, PeanoError>(Peano peano) -> peano;
-            case Result.Err<Peano, PeanoError>(PeanoError error) -> throw new IllegalArgumentException(error.toString());
+            case Ok<Peano, PeanoError>(Peano peano) -> peano;
+            case Err<Peano, PeanoError>(PeanoError error) -> throw new IllegalArgumentException(error.toString());
         };
 
         final Peano num4 = PeanoImpl.add(num3, PeanoImpl.fromInt(2));
