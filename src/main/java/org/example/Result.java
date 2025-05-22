4f06eab0626d745b778cdb67268a39b78546d812
William Raendchen <william.raendchen@gmail.com>
2025-05-23 00:00:00 +0200

    cleanup

--- src/main/java/org/example/Result.java
+++ src/main/java/org/example/Result.java
@@ -2,19 +2,20 @@ package org.example;
 
 @SuppressWarnings("unused")
 sealed public interface Result<T, E> {
-    T getOrThrow() throws RuntimeException;
+    record Ok<T, E>(T value) implements Result<T, E> {}
+    record Err<T, E>(E error) implements Result<T, E> {}
 
-    record Ok<T, E>(T value) implements Result<T, E> {
-        @Override
-        public T getOrThrow() {
-            return value;
-        }
+    default T getOrThrow() throws RuntimeException {
+        return switch (this) {
+            case Ok(T value) -> value;
+            case Err(E err) -> throw new RuntimeException(err.toString());
+        };
     }
 
-    record Err<T, E>(E error) implements Result<T, E> {
-        @Override
-        public T getOrThrow() throws RuntimeException {
-            throw new RuntimeException(error.toString());
-        }
+    default T getOrElse(T defaultValue) {
+        return switch (this) {
+            case Ok(T value) -> value;
+            case Err(E err) -> defaultValue;
+        };
     }
 }
